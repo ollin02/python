5ba599c9f6cc5b63a28b86542c202ee74ddaa1e6
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-04 10:36:04 -0600

    NOTES: uso dela funcion map y el metodo .append

--- Avanzado/python-avanzado-2441006-main/04_LIST_COMPREHENSION/04_01_map.py
+++ Avanzado/python-avanzado-2441006-main/04_LIST_COMPREHENSION/04_01_map.py
@@ -1,15 +1,18 @@
+#funcio map retorna un objeto de tipo map es un iterador 
 def calcular_cuadrado(numero):
     return numero**2
 
 
 lista_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-lista_cuadrados = []
+lista_cuadrados = [] # esto sirve para utilizar el cilo for linea 7 a la 13
 
 for num in lista_num:
     cuadrado = calcular_cuadrado(num)
-    lista_cuadrados.append(cuadrado)
+    lista_cuadrados.append(cuadrado)#aqui agregamos el numero a la lista de cuadrados
+                                    #.append(nombre, tipo) es un metodo que sirve para agregar al gun 
+                                    #elemento a una lista
 
 print(lista_cuadrados)
 
-map_cuadrados = list(map(calcular_cuadrado, lista_num))
+map_cuadrados = list(map(calcular_cuadrado, lista_num)) # con la funcion map se simplifica la sintaxis
 print(map_cuadrados)
