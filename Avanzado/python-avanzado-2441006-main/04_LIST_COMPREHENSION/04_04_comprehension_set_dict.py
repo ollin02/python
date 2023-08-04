67ef16edc46f158824521ed6a7cbf12b65d1388b
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-04 11:15:19 -0600

    NOTES: uso de list comprehension set y diccionario

--- Avanzado/python-avanzado-2441006-main/04_LIST_COMPREHENSION/04_04_comprehension_set_dict.py
+++ Avanzado/python-avanzado-2441006-main/04_LIST_COMPREHENSION/04_04_comprehension_set_dict.py
@@ -1,8 +1,13 @@
 lista_num = [1, 2, 3, 4, 1, 2, 5, 6, 8]
 
-set_pares = {num for num in lista_num if num % 2==0}
-print(set_pares)
+set_pares = {num for num in lista_num if num % 2==0}# las llaves nos indicas que es un set o diccionarios
+print(set_pares)# Los sets no contienen valores repetidos
 
 vocales = "aeiou"
-diccionario = {vocal.lower(): vocal.upper() for vocal in vocales}
+                #{llave:valor}
+diccionario = {vocal.lower(): vocal.upper() for vocal in vocales}#Diccionario se definen llaves y valor
+#.lower() nos convierte un texto en minusculas
+#.upper() nos convierte un texto  en mayusculas
+#vocal.lower() es la llave
+#vocal.upper() es el valor
 print(diccionario)
