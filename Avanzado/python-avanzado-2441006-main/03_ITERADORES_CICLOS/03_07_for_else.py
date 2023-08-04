5c5dae1ae5cb3f54a558d1a1f0bbb193430a65c7
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-04 10:18:18 -0600

    NOTES: uso de break en el ciclo for

--- Avanzado/python-avanzado-2441006-main/03_ITERADORES_CICLOS/03_07_for_else.py
+++ Avanzado/python-avanzado-2441006-main/03_ITERADORES_CICLOS/03_07_for_else.py
@@ -1,8 +1,8 @@
 lista_nombres = ["Paco", "Emilio", "Javier", "Ana"]
 
-for nombre in lista_nombres:
+for nombre in lista_nombres: 
     print(nombre)
-    if nombre == "Javier":
-        break
+    if nombre == "Javier": #Si encuentra a Javier termina el ciclo
+        break #rompe el ciclo
 else:
     print("Ciclo terminado")
