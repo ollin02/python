f0eb2bd521f3334686a227d9cb1d265fc1851480
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-04 10:47:53 -0600

    NOTES: uso de list comprehension

--- Avanzado/python-avanzado-2441006-main/04_LIST_COMPREHENSION/04_03_list_comprehension_condicion.py
+++ Avanzado/python-avanzado-2441006-main/04_LIST_COMPREHENSION/04_03_list_comprehension_condicion.py
@@ -1,3 +1,4 @@
+# forma corta y elegante de generar uan lista en python
 """
 lista = [ expresion(elemento) for elemento in objeto_iterable if condición ]
 lista = [ expresion(condición) for elemento in objeto_iterable ]
@@ -12,11 +13,22 @@ def es_par(numero):
 
 
 lista_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#sintaxis antigüa
+lista_cuadrados=[]
+for num in lista_num:
+    cuadrados = num**2
+    lista_cuadrados.append(cuadrados)
+
+print("Cilco for: ", lista_cuadrados)
+#fin del metoso antigüo
+
+#nueva forma con listcomprehension
 lista_cuadrados = [calcular_cuadrado(num) for num in lista_num]
-print(lista_cuadrados)
+print("List comprehension: ",lista_cuadrados)
 
 lista_cuadrados_pares = [calcular_cuadrado(num) for num in lista_num if es_par(num)]
-print(lista_cuadrados_pares)
+print("Cuadrados pares: ",lista_cuadrados_pares)
 
+#verifica si es par o impar y pone un cero cuanso el numerp es impar
 lista_resultados = [calcular_cuadrado(num) if es_par(num) else 0 for num in lista_num]
 print(lista_resultados)
