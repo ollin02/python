50c80e9029489f8c7be40f62ca1ad5b2fd2e9496
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-04 11:43:45 -0600

    NOTES: mal uso de list comprehension

--- Avanzado/python-avanzado-2441006-main/04_LIST_COMPREHENSION/04_06_mal_uso_comprehension.py
+++ Avanzado/python-avanzado-2441006-main/04_LIST_COMPREHENSION/04_06_mal_uso_comprehension.py
@@ -1,19 +1,19 @@
 # Caso 1
-total = sum([num for num in range(100)])
+total = sum([num for num in range(100)]) #List comprehension
 # Corección 
-total = sum(num for num in range(100))
+total = sum(num for num in range(100)) # se puede crear directamente en la funcion sum
 
 
 # Caso 2
-[print(element) for element in range(1)]
+[print(element) for element in range(10)] #imprimir una variable de un objeto iterable
 # Corección 
 for element in range(10):
     print(element)
 
 # Caso 3
-lista_anidada = [[1, 2], [3, 4], [5, 6]]
+lista_anidada = [[1, 2], [3, 4], [5, 6]] 
 
-valores_lista = [numero for elemento in lista_anidada for numero in elemento]
+valores_lista = [numero for elemento in lista_anidada for numero in elemento]# no es cobeniente anidar dentro de list comprehension
 print(valores_lista)
 
 # Corección 
