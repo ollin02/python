1d3ba5d36f50cb6858c84fa283c4322890a0d07e
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-04 10:13:53 -0600

    NOTES: comentarios

--- Avanzado/python-avanzado-2441006-main/03_ITERADORES_CICLOS/03_06_enumerate.py
+++ Avanzado/python-avanzado-2441006-main/03_ITERADORES_CICLOS/03_06_enumerate.py
@@ -1,10 +1,14 @@
 """
-enumerate(iterable, start=0)
+enumerate(iterable, start=0) start es donde iniciara el elemento iterable, puede empezar con el numero que quieras
 """
 
 nombres = ["Paco", "Emilio", "Javier", "Ana"]
 nombres_enumerados = enumerate(nombres, start=5)
-print(list(nombres_enumerados))
-
+print(list(nombres_enumerados)) #se pone la función list para que aparesca en formato de lista en la imprecion 
+                                #tambien se pone porque al imprimirlo aparece como un objeto de tipo enumerate al hacer la impreción
+                                #imprime como una dupla
 for indice, elemento in enumerate(nombres):
-    print(indice, elemento)
+    print(indice, elemento) 
+
+for indice, elemento in enumerate(nombres):# prueba
+    print(indice+5, elemento)
