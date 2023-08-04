fecf6034a08554dc21dc813c0afce8a6050bf05e
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-04 12:07:47 -0600

    NOTES: creación de archivo csv

--- Avanzado/python-avanzado-2441006-main/05_ARCHIVOS_CSV/05_01_crear_csv.py
+++ Avanzado/python-avanzado-2441006-main/05_ARCHIVOS_CSV/05_01_crear_csv.py
@@ -2,12 +2,15 @@ import csv
 import os
 
 ruta = "csv_vacio.csv"
-ruta_absoluta = "C:\\Users\\Ana\\Documents\\PythonAvanzado\\05_ARCHIVOS_CSV\\csv_vacio.csv"
+ruta_absoluta = "D:\\Usuario_Ollin\\Documents\\CV_Ollin\\Curso_Python\\Avanzado\\python-avanzado-2441006-main\\05_ARCHIVOS_CSV\\csv_vacio.csv"
 ruta_absoluta_os = os.path.join(os.getcwd(), "csv_vacio.csv")
 
 print(ruta_absoluta)
 print(ruta_absoluta_os)
 
-archivo_abierto = open(ruta_absoluta_os, "w")
+
+archivo_abierto =open(ruta,"w")
+#archivo_abierto = open(ruta_absoluta, "w")
+#archivo_abierto = open(ruta_absoluta_os, "w")
 writer = csv.writer(archivo_abierto, delimiter=",")
 archivo_abierto.close()
