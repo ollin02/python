432c58547bd394ec8dfc148c0d4d47748a065a97
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-05 11:26:47 -0600

    Notes: Guardando los logging en un archivo

--- Avanzado/python-avanzado-2441006-main/06_LOGGING/06_02_logs.py
+++ Avanzado/python-avanzado-2441006-main/06_LOGGING/06_02_logs.py
@@ -1,9 +1,12 @@
 import logging
 
+#eligimos el nivel del log
+logging.basicConfig(level=logging.DEBUG)
+#normalmente se guardan en archivos
 logging.basicConfig(level=logging.DEBUG, filename="ejemplo_logs.log", filemode="w")
 
-logging.debug("Log de debugging") 
-logging.info("Log informativo")
-logging.warning("Log de advertencia")
-logging.error("Log de error")
-logging.critical("Log de error crítico")
+logging.debug("Log de debugging") # no se imprimen estos errore porque no son de primer nivel
+logging.info("Log informativo") # no se imprimen estos errore porque no son de primer nivel
+logging.warning("Log de advertencia") # WARNNING: root: Log de advertencia
+logging.error("Log de error") # ERROR: root: Log de error
+logging.critical("Log de error crítico") # CRITICAL: root: Log de error crítico
