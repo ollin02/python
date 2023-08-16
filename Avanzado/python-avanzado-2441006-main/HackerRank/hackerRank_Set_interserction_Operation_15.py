f5edd98da8349b244e17fdf6e5e8445e687c6a54
Ollin <tonatiuh.ollin.gomez@gmail.com>
2023-08-15 22:04:02 -0600

    ADVANCE: metodo intersection

---
+++ Avanzado/python-avanzado-2441006-main/HackerRank/hackerRank_Set_interserction_Operation_15.py
@@ -0,0 +1,10 @@
+#programa que une dos cadenas y genera una nueva con la interceccion etre ellas y 
+#cuenta el numero de valores que tiene la nueva cadena
+n=int(input("Escribe el número de personas inscritas al periodico ingles: "))
+print("Ingrese el numero de lista de los estudiantes inscritos: ")
+k=set(map(int, input().split()))
+m=int(input("Escribe el número de personas inscritas al periodico frances: "))
+print("Ingrese el numero de lista de los estudiantes inscritos: ")
+l=set(map(int,input().split()))
+
+print(f"número total de personas: ",len(k&(l)))
